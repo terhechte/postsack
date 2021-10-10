158b6995a7edb3f2d65d7ac604512a30d62e2c9d
Benedikt Terhechte <terhechte@gmail.com>
2021-10-10 08:05:56 +0200

    Some cleanup

--- src/importer/formats/gmailbackup/mod.rs
+++ src/importer/formats/gmailbackup/mod.rs
@@ -1,8 +1,9 @@
-mod filesystem;
 mod meta;
 mod raw_email;
 
+use super::shared::filesystem::{emails_in, folders_in};
 use super::{Config, ImporterFormat, MessageSender, Result};
+use raw_email::RawEmailEntry;
 
 #[derive(Default)]
 pub struct Gmail {}
@@ -10,6 +11,10 @@ pub struct Gmail {}
 impl ImporterFormat for Gmail {
     type Item = raw_email::RawEmailEntry;
     fn emails(&self, config: &Config, sender: MessageSender) -> Result<Vec<Self::Item>> {
-        filesystem::read_emails(config, sender)
+        Ok(folders_in(
+            &config.emails_folder_path,
+            sender,
+            |path, sender| emails_in(path, sender, RawEmailEntry::new),
+        )?)
     }
 }
